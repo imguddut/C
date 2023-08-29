c48d653ecb622a6d1d1dea89d4f0f481fe9b5d57
NAVAL KISHOR KUMAR <97917353+imguddut@users.noreply.github.com>
2023-08-29 08:48:58 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b,c;
+    printf("Enter two number");
+    scanf("%d %d", &a , &b);
+    c=a+b;
+    printf("Sum is %d", c);
+
+    return 0;
+}
