36408b4b21aae708a0d5cf2c8ee528970f484068
NAVAL KISHOR KUMAR <97917353+imguddut@users.noreply.github.com>
2023-08-31 23:51:43 +0530

    Add files via upload

---
+++ greaterof2usingif.c
@@ -0,0 +1,21 @@
+//greater of 2 no. using if else//
+
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    printf("enter 2 no.");
+    scanf("%d %d", &a, &b);
+
+    if (a>b)
+    {
+    printf("A is greater");
+
+    }
+    else{
+        printf("B is greater");
+    }
+
+
+    return 0;
+}
