ac28661c4b5f9e1d594adde19aee5a6c44639912
NAVAL KISHOR KUMAR <97917353+imguddut@users.noreply.github.com>
2023-09-01 00:01:27 +0530

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,20 @@
+// even or odd//
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a;
+    printf("Enter a number \n");
+    scanf("%d", &a);
+
+if (a%2==0)
+{
+    printf("a is even");
+
+}
+else
+{
+    printf("a is odd");
+}
+
+    return 0;
+}
