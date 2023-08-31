3d43eb6f13721c5b37920d6c63f87f43cc7b6c5e
NAVAL KISHOR KUMAR <97917353+imguddut@users.noreply.github.com>
2023-08-31 23:57:11 +0530

    Add files via upload

---
+++ +veor-ve.c
@@ -0,0 +1,20 @@
+// +ve or -ve using c//
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a;
+
+    printf("Enter a Number ");
+    scanf("%d", &a);
+
+if (a>0)
+{
+    printf("A is positive");
+}
+else
+{
+    printf("A is negative");
+}
+
+    return 0;
+}
